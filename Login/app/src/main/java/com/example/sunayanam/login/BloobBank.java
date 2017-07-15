c8cfc098a33e8418120573cefa9f2680053064c6
Sunayana1996 <sunayanamolanguri@gmail.com>
2017-07-16 02:13:10 +0530

    Receipent List
    REceipent List

--- Login/app/src/main/java/com/example/sunayanam/login/BloobBank.java
+++ Login/app/src/main/java/com/example/sunayanam/login/BloobBank.java
@@ -1,7 +1,10 @@
 package com.example.sunayanam.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BloobBank extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class BloobBank extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bloob_bank);
-        
+        final Button bMap = (Button) findViewById(R.id.bMap);
+
+        bMap.setOnClickListener(new View.OnClickListener() {
+                                      public void onClick(View view) {
+                                          Intent bLogin = new Intent(BloobBank.this, Map.class);
+                                          BloobBank.this.startActivity(bLogin);
+                                      }
+                                  }
+        );
+    }
+
+
     }
 }
