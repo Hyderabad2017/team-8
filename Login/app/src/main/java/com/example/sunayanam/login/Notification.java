53e04abc5cd71f358bee51c3d07a0764ec1c293a
Sunayana1996 <sunayanamolanguri@gmail.com>
2017-07-16 00:04:39 +0530

    Donator-Update
    Donator-Update

---
+++ Login/app/src/main/java/com/example/sunayanam/login/Notification.java
@@ -0,0 +1,26 @@
+package com.example.sunayanam.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Notification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        final Button bCheck = (Button) findViewById(R.id.bLogin);
+
+        bCheck.setOnClickListener(new View.OnClickListener() {
+                                      public void onClick(View view) {
+                                          Intent bLogin = new Intent(Notification.this, BloobBank.class);
+                                          Notification.this.startActivity(bLogin);
+                                      }
+                                  }
+        );
+    }
+
+}
