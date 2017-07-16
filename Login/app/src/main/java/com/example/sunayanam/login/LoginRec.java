17ec22ff10a417444539b749361f8148174d328f
Sunayana1996 <sunayanamolanguri@gmail.com>
2017-07-16 10:29:43 +0530

    Recipent changes
    Recipent changes

---
+++ Login/app/src/main/java/com/example/sunayanam/login/LoginRec.java
@@ -0,0 +1,58 @@
+package com.example.sunayanam.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LoginRec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_rec);
+                final EditText etUsername =(EditText) findViewById(R.id.etUsername);
+                final EditText etPassword =(EditText) findViewById(R.id.etPassword);
+
+                final Button bLogin = (Button) findViewById(R.id.bLogin);
+                final TextView registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+
+                registerLink.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent registerIntent = new Intent(LoginRec.this,RegisterRec.class);
+                        LoginRec.this.startActivity(registerIntent);
+                    }
+                });
+
+                try {
+                    URL url = new URL("10.49.190.144/unitedway/donorregister.php?pwd=etPassword+un=etUsername");
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                }
+                catch (Exception e) {
+                }
+
+                bLogin.setOnClickListener(new View.OnClickListener()
+                                          {
+                                              public void onClick(View view)
+                                              {
+                                                  Intent bLogin = new Intent (LoginRec.this,Bloodgroup.class);
+                                                  LoginRec.this.startActivity(bLogin);
+                                              }
+                                          }
+                );
+
+            }
+        }
+
+    }
+}
---
+++ Login/app/src/main/java/com/example/sunayanam/login/RegisterRec.java
@@ -0,0 +1,13 @@
+package com.example.sunayanam.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterRec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_rec);
+    }
+}
